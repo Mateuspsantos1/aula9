b801a9d1cc6c11995f90c8b4f4bfa4a079d5cdc9
Yangmape <134799815+Mateuspsantos1@users.noreply.github.com>
2023-07-03 22:03:17 -0300

    Add files via upload

---
+++ entrega aula 9/index.js
@@ -0,0 +1,14 @@
+let nomeUsuario = document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+        
+        const user = document.querySelector("#dados");
+      
+      user.click();
+    
+       
+    
+    }
+  });
+
+  console.log("seja bem vindo usuario")
+
